f1c538c3d695c3afef82dc7100e2e52e37292cbf
wangwq <wangwq@mail.jj.cn>
2020-10-20 18:35:01 +0800

    scsdcv

--- test_selenium.py
+++ test_selenium.py
@@ -1,28 +1,9 @@
-import os
-from time import sleep
 
-import pytest
-import selenium
 from selenium import webdriver
 
 
-# def setup(self):
-#     self.driver = webdriver.Chrome()
-#     self.driver.maximize_window()
-#
-#
-# def teardown(self):
-#     sleep(2)
-#     self.driver.quit()
 
-@pytest.fixture(scope="module")
-def kj():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    yield
-    sleep(2)
-    driver.quit()
-
-
-def test_selenium(self):
+def test_hogwarts(self):
+    self.driver = webdriver.Chrome()
+    self.driver.maximize_window()
     self.driver.get("https://www.baidu.com/")
