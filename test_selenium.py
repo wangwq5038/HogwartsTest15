0a7597534a044d730544f67ad1d9e7c0491bf707
wangwq <wangwq@mail.jj.cn>
2020-10-22 18:36:05 +0800

    修改test_selenium

--- test_selenium.py
+++ test_selenium.py
@@ -27,7 +27,7 @@
 # action.release(on_element=None)
 # action.send_keys(*keys_to_send)
 # action.send_keys_to_element(element, *keys_to_send)
-
+from time import sleep
 
 from selenium import webdriver
 
@@ -40,9 +40,11 @@ from selenium import webdriver
 
 # class TestHogwarts:
 #     def setup_method(self, method):
-#         self.driver = webdriver.Chrome()
-#         self.driver.implicitly_wait(3)
+#         # self.driver = webdriver.Chrome()
+#         self.driver = webdriver.Firefox()
+#         self.driver.implicitly_wait(5)
 #     def teardown_method(self, method):
+#         sleep(3)
 #         self.driver.quit()
 #
 #
@@ -53,21 +55,22 @@ from selenium import webdriver
 #         self.driver.find_element_by_partial_link_text('测试开发工程师的黄埔军校').click()
 #         handles = self.driver.window_handles
 #         print(handles)
-#         self.driver.switch_to.window(handles[-1])
-#         self.driver.find_element_by_partial_link_text('近期内推').click()
+#         # self.driver.switch_to.window(handles[-1])
+#         sleep(2)
+#         self.driver.find_element_by_link_text('技术服务').click()
 
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-
-class TestWework:
-    def setup(self):
-        options = webdriver.ChromeOptions()
-        options.debugger_address = "127.0.0.1:9222"
-        self.driver = webdriver.Chrome(options=options)
-        self.driver.implicitly_wait(2)
-
-    def test_upload(self):
-        element_add = self.driver.find_element(By.CSS_SELECTOR, ".js_upload_file_selector")
-        self.driver.execute_script("arguments[0].click();", element_add)
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+#
+#
+# class TestWework:
+#     def setup(self):
+#         options = webdriver.ChromeOptions()
+#         options.debugger_address = "127.0.0.1:9222"
+#         self.driver = webdriver.Chrome(options=options)
+#         self.driver.implicitly_wait(2)
+#
+#     def test_upload(self):
+#         element_add = self.driver.find_element(By.CSS_SELECTOR, ".js_upload_file_selector")
+#         self.driver.execute_script("arguments[0].click();", element_add)
